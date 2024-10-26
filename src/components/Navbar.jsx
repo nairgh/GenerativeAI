3bc6b4936719be3da8001f01bc7341a1e3f80bea
Hari Nair <nairgh@hotmail.com>
2024-10-25 23:40:26 -0400

    update buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
               </a>
               <a
                 href="#"
-                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-lime-500 to-orange-800"
               >
                 Create an account
               </a>
